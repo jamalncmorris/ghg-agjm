0c19220f5c9cca669f3fc40f894646b8c3b06c80
Jamal N.C Morris <jamalncmorris@gmail.com>
2026-01-08 17:57:52 +0000

    22:22

--- landing_page/src/LandingPage.jsx
+++ landing_page/src/LandingPage.jsx
@@ -132,7 +132,7 @@ export default function LandingPage() {
                 className="h-11 not-dark:hidden"
               />
               <div className="mt-24 sm:mt-32 lg:mt-16">
-                <a href="#" className="inline-flex space-x-6">
+                <div className="inline-flex space-x-6">
                   <span className="rounded-full bg-indigo-50 px-3 py-1 text-sm/6 font-semibold text-indigo-600 ring-1 ring-indigo-600/20 ring-inset dark:bg-indigo-500/10 dark:text-indigo-400 dark:ring-indigo-500/25">
                     What's new
                   </span>
@@ -140,24 +140,30 @@ export default function LandingPage() {
                     <span>App in Development</span>
                     <ChevronRightIcon aria-hidden="true" className="size-5 text-gray-400 dark:text-gray-500" />
                   </span>
-                </a>
+                </div>
               </div>
               <h1 className="mt-10 text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-7xl dark:text-white">
-                Find legal cases fast
+                Find cases fast
               </h1>
               <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">
-                Our platform helps you find legal cases quickly and efficiently. Browse Live cases, Upload your active cases. 
-                Find new clients and grow your practice with our intuitive tools. 
+                Our platform offers a quick and effective legal case workflow. Browse Live cases, Upload active cases. Find new clients and grow your practice with our intuitive tools. 
               </p>
               <div className="mt-10 flex items-center gap-x-6">
                 <a
-                  href="#"
+                  href="https://chat.whatsapp.com/FG3kZwTpSad3gxGuHhkeuI"
+                  target="_blank" 
+                  rel="noopener noreferrer"
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
                 >
                   Get started
                 </a>
-                <a href="#" className="text-sm/6 font-semibold text-gray-900 dark:text-white">
-                  Learn more <span aria-hidden="true">→</span>
+                <a 
+                  href="https://chat.whatsapp.com/FG3kZwTpSad3gxGuHhkeuI"
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                  className="text-sm/6 font-semibold text-gray-900 dark:text-white"
+                >
+                  Join for updates <span aria-hidden="true">→</span>
                 </a>
               </div>
             </div>
